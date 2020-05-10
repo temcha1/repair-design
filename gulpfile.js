7766b35eab6b843b1f21a47309152d33b3201c87
unknown <temchamaiser@gmail.com>
2020-05-10 12:35:46 +0300

    hero block bem

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+var cleanCSS     = require('gulp-clean-css');
+var rename       = require("gulp-rename");
 
 gulp.task('hello', function(done){
   console.log('Привет');
@@ -14,4 +16,17 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+gulp.task('mincss', function() {
+
+  return gulp.src("./*.css")
+  
+  .pipe(rename({suffix: ".min"}))
+  
+  .pipe(cleanCSS())
+  
+  .pipe(gulp.dest("app/css"));
+  
+  })
+  
 
